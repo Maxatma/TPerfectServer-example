1af9a3f8ee32bf2f034363bf929a8aab16e55cf2
Alexander Zaporozhchenko <maxatma@mail.ua>
2018-11-02 20:21:41 +0200

    Add gitignore for Package.resolved Update package description api to v4.2

--- Package.swift
+++ Package.swift
@@ -7,16 +7,23 @@ import PackageDescription
 let package = Package(
     name: "test server",
     dependencies: [
-        .package(url: "https://github.com/apocolipse/TPerfectHTTPServer.git", from: "2.0.0"),
+        .package(url: "https://github.com/Maxatma/TPerfectHTTPServer.git", from: "2.0.0"),
         .package(url: "https://github.com/apocolipse/Thrift-Swift.git", from: "1.0.0")
-        ],
+    ],
     targets: [
         .target(
             name: "test server",
-            dependencies: ["TPerfectHTTPServer", "Thrift"]),
+            dependencies: [
+                "TPerfectHTTPServer",
+                "Thrift",
+                ]),
         .testTarget(
             name: "test serverTests",
-            dependencies: ["test server","Thrift"]),
-    ]
+            dependencies: [
+                "test server",
+                "Thrift"
+            ]),
+        ],
+    swiftLanguageVersions: [.v4_2]
 )
 
